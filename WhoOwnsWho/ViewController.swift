a1484e2e0428dd0fbeda2ef03fd6a879680930cb
nuh1234 <nuh.mohamud@gmail.com>
2018-03-03 13:54:30 -0500

    Initial Commit

---
+++ WhoOwnsWho/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WhoOwnsWho
+//
+//  Created by Nuh Mohamud on 3/3/18.
+//  Copyright © 2018 Nuh Mohamud. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
